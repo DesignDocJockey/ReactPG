f7cb4bf41cd56dbc40cf6cf8089002fb177733ec
hew0n0 <hwong18@gmail.com>
2018-07-04 22:45:34 -0400

    Added Scoreboard component

---
+++ src/components/score.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default class Score extends React.Component {
+    constructor(props) {
+        //calls the parent React.Component() to get the 'this' reference
+        super(props);          
+        //initialize state in the constructor         
+        this.state = {score: 0};
+        //make 'this' available on click handlers
+        this.incrementScore = this.incrementScore.bind(this);
+        this.decrementScore = this.decrementScore.bind(this);
+    }
+
+    incrementScore() {
+        console.log('increment button clicked');
+        this.setState({
+            score: this.state.score + 1
+        });
+    }
+
+    decrementScore() {
+        console.log('decrement button clicked');
+        this.setState({
+            score: this.state.score - 1
+        });
+    }
+    
+    render() {
+        return (
+            <div>
+                <h2>Score board for {this.props.teamName}</h2>
+                <div>
+                        Score: {this.state.score} <br/>
+                        <button onClick={this.incrementScore}>+</button>
+                        <button onClick={this.decrementScore}>-</button>
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/helloworld';
+import Score from './components/score' 
 
+//ReactDOM.render(<Score teamName="Colts"/> , document.querySelector('.container')); 
 ReactDOM.render(<App /> , document.querySelector('.container')); 
 
 //http://localhost:8080/index
